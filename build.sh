1b2954d18ad2c6b389acf8e9984581e77ba78436
Manuel Reinhardt <manuel.rhdt@gmail.com>
2018-02-04 13:49:37 +0000

    M  build.sh M  log M  www.physik.uni-muenchen.de/lehre/vorlesungen/wise_17_18/E5/folien/index.html M  www.physik.uni-muenchen.de/lehre/vorlesungen/wise_17_18/E5/uebungen/index.html M  www.physik.uni-muenchen.de/lehre/vorlesungen/wise_17_18/E6_p/uebung/index.html M  www.physik.uni-muenchen.de/lehre/vorlesungen/wise_17_18/E6_p/vorlesung/index.html M  www.physik.uni-muenchen.de/lehre/vorlesungen/wise_17_18/T4_stat_mech/index.html M  www.physik.uni-muenchen.de/lehre/vorlesungen/wise_17_18/T4_stat_mech/materialien/index.html M  www.physik.uni-muenchen.de/lehre/vorlesungen/wise_17_18/T4_stat_mech/uebungen/index.html M  www.physik.uni-muenchen.de/lehre/vorlesungen/wise_17_18/tvi_tc1_gr/Tutorials/index.html M  www.physik.uni-muenchen.de/lehre/vorlesungen/wise_17_18/tvi_tc1_gr/vorlesung/index.html

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-wget -r --convert-links --backup-converted --timestamping -np --http-user=Manuel.Reinhardt --http-password=1742MaRe@ --input=urls -o log
+wget -r -w 1 --convert-links --backup-converted --adjust-extension --timestamping --no-parent --http-user=Manuel.Reinhardt --http-password=1742MaRe@ --input=urls -o log
 
 git add --all
 git status --porcelain | git commit -F -
