5489c6a59677ebe51bc3d804cec80de4aff76c22
Manuel Reinhardt <manuel.rhdt@gmail.com>
2018-02-04 13:30:28 +0000

    M  build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ set -euo pipefail
 IFS=$'\n\t'
 
 wget -kr --timestamping -np --http-user=Manuel.Reinhardt --http-password=1742MaRe@ --input=urls
+
+git add --all
+git status --porcelain | git commit -F -
