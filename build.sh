712b6a512f365abcc3820652f3c7dadaa0d0eb27
Manuel Reinhardt <manuel.rhdt@gmail.com>
2018-02-04 15:54:56 +0000

    Add email sending

--- build.sh
+++ build.sh
@@ -4,5 +4,10 @@ IFS=$'\n\t'
 
 wget -r -w 1 --convert-links --backup-converted --timestamping --no-parent --http-user=Manuel.Reinhardt --http-password=1742MaRe@ --input=urls -o log
 
-git add --all
-git status --porcelain | git commit -F -
+if [ -n "$(git status --porcelain)" ]; then
+	git add --all
+	git status --porcelain | git commit -F -
+	git send-email -1
+fi
+
+
